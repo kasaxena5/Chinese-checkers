82f24dfdb92a7f82bab6b8ede89954c99df393d1
Kartikeya Saxena <kasaxena@microsoft.com>
2023-02-28 21:14:31 +0530

    Logic completed

--- Assets/Scripts/ChineseChecker.cs
+++ Assets/Scripts/ChineseChecker.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChineseChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum ChineseState
     {
-        
+        Blank,
+        Red,
+        Blue,
+        Potential
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private ChineseState state = ChineseState.Blank;
+
+    public int i;
+    public int j;
+
+    public void SetState(ChineseState newState)
     {
-        
+        state = newState;
+        this.GetComponent<Image>().color = state switch
+        {
+            ChineseState.Blank => Color.white,
+            ChineseState.Red => Color.red,
+            ChineseState.Blue => Color.blue,
+            ChineseState.Potential => Color.yellow,
+            _ => Color.white,
+        };
     }
+
+    public ChineseState GetState()
+    {
+        return state;
+    }
+
 }
--- Assets/Scripts/ChineseCheckersManager.cs
+++ Assets/Scripts/ChineseCheckersManager.cs
@@ -12,11 +12,12 @@ public class ChineseCheckersManager : MonoBehaviour
     private Transform canvas;
 
     [SerializeField]
-    private int boardSizeX;
+    private int boardSizeX = 20;
     [SerializeField]
-    private int boardSizeY;
+    private int boardSizeY = 20;
 
-    private ChineseChecker[] checkers;
+    private ChineseChecker activeChecker = null;
+    private Queue<ChineseChecker> potentialMoves = new Queue<ChineseChecker>();
 
     private ChineseChecker[,] checkerBoard;
 
@@ -27,18 +28,31 @@ public class ChineseCheckersManager : MonoBehaviour
         {
             for(int j = 0; j < boardSizeY; j++)
             {
-                if ((i + j) % 2 == 0)
+                if ((i + j) % 2 == 0 && (i + j) >= 10 && (i + j) <= 28 && (i - j) >= -8 && (i - j) <= 10)
                 {
                     checkerBoard[i, j] = Instantiate(checkersPrefab);
+                    checkerBoard[i, j].i = i;
+                    checkerBoard[i, j].j = j;
                     checkerBoard[i, j].transform.SetParent(canvas, false);
-                    checkerBoard[i, j].GetComponent<RectTransform>().localPosition = new Vector3(40 * i, 40 * j, 0);
+                    checkerBoard[i, j].GetComponent<RectTransform>().localPosition = new Vector3(20 * i - 200, 20 * j - 200, 0);
+                    if(j >= 14)
+                    {
+                        checkerBoard[i, j].SetState(ChineseChecker.ChineseState.Red);
+                    }
+                    else if(j <= 4)
+                    {
+                        checkerBoard[i, j].SetState(ChineseChecker.ChineseState.Blue);
+                    }
+                    int x = i;
+                    int y = j;
+                    checkerBoard[i, j].GetComponent<Button>().onClick.AddListener(() => { OnClick(x, y); });
                 }
 
             }
         }
     }
 
-    public void DestroyCheckerBoard()
+    private void DestroyCheckerBoard()
     {
         if (checkerBoard == null)
             return;
@@ -53,6 +67,14 @@ public class ChineseCheckersManager : MonoBehaviour
                 }
             }
         }
+
+        activeChecker = null;
+    }
+
+    public void ResetCheckerBoard()
+    {
+        DestroyCheckerBoard();
+        InstantiateCheckerBoard();
     }
 
     private void Awake()
@@ -60,18 +82,102 @@ public class ChineseCheckersManager : MonoBehaviour
         InstantiateCheckerBoard();   
     }
 
-    void Start()
+    private bool IsEmpty(int i, int j)
     {
-        //checkers = new ChineseChecker[2];
-        //checkers[0] = Instantiate(checkersPrefab);
-        //checkers[0].transform.SetParent(canvas, false);
-        //checkers[0].GetComponent<RectTransform>().localPosition = new Vector3(10, 10, 0);
-        //checkers[0].GetComponent<RectTransform>().localPosition = new Vector3(0, 10, 0);
+        if (checkerBoard[i, j] == null)
+            return false;
+        return (checkerBoard[i, j].GetState() == ChineseChecker.ChineseState.Blank);
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsOccupied(int i, int j)
     {
-        
+        if (checkerBoard[i, j] == null)
+            return false;
+        return (checkerBoard[i, j].GetState() == ChineseChecker.ChineseState.Red || checkerBoard[i, j].GetState() == ChineseChecker.ChineseState.Blue);
     }
+
+    private void MoveChecker(int i, int j)
+    {
+        checkerBoard[i, j].SetState(activeChecker.GetState());
+        checkerBoard[activeChecker.i, activeChecker.j].SetState(ChineseChecker.ChineseState.Blank);
+        activeChecker = null;
+    }
+
+    private void ClearPotentialMoves()
+    {
+        while(potentialMoves.Count != 0)
+        {
+            ChineseChecker c = potentialMoves.Dequeue();
+            c.SetState(ChineseChecker.ChineseState.Blank);
+        }
+    }
+
+    public void OnClick(int i, int j)
+    {
+        if(activeChecker == checkerBoard[i, j])
+        {
+            activeChecker = null;
+            ClearPotentialMoves();
+            return;
+        }
+
+        if (!IsOccupied(i, j) && !IsEmpty(i, j))
+        {
+            ClearPotentialMoves();
+            MoveChecker(i, j);
+            return;
+        }
+
+        if (IsOccupied(i, j))
+        {
+            activeChecker = checkerBoard[i, j];
+            Queue<ChineseChecker> q = new Queue<ChineseChecker>();
+
+            // Direct Moves
+            int[] dx = {1, -1, 1, -1 };
+            int[] dy = { 1, -1, -1, 1 };
+            for(int k = 0; k < 4; k++)
+            {
+                int x = i + dx[k];
+                int y = j + dy[k];
+                if (x >= 0 && y >= 0 && x < boardSizeX && y < boardSizeY)
+                {
+                    if (IsEmpty(x, y))
+                    {
+                        potentialMoves.Enqueue(checkerBoard[x, y]);
+                        checkerBoard[x, y].SetState(ChineseChecker.ChineseState.Potential);
+                    }
+                }
+            }
+
+            // Indirect Moves
+            q.Enqueue(checkerBoard[i, j]);
+            while(q.Count != 0)
+            {
+                ChineseChecker c =  q.Dequeue();
+                for(int k = 0; k < 4; k++)
+                {
+                    int x = c.i + 2 * dx[k];
+                    int y = c.j + 2 * dy[k];
+
+                    int x_ = c.i + dx[k];
+                    int y_ = c.j + dy[k];
+
+                    if(IsOccupied(x_, y_) && IsEmpty(x, y))
+                    {
+                        q.Enqueue(checkerBoard[x, y]);
+                        potentialMoves.Enqueue(checkerBoard[x, y]);
+                        checkerBoard[x, y].SetState(ChineseChecker.ChineseState.Potential);
+
+                    }
+                    
+
+                }
+                
+            }
+        }
+        
+
+    }
+    
 }
